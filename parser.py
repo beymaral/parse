2675f593aebbba413173df4b62c9aa78949d2d47
beymaral <usultankulov@gmail.com>
2022-07-15 10:34:20 +0600

    add parser

---
+++ parser.py
@@ -0,0 +1,38 @@
+import requests
+from bs4 import BeautifulSoup
+
+URL = 'https://www.kivano.kg/mobilnye-telefony'
+HEADERS = {
+    'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.5 Safari/605.1.15',
+    'accept': '*/*',
+}
+LINK = 'https://www.kivano.kg'
+
+
+
+def get_html(headers, url, params=None):
+    response = requests.get(url, params=params, headers=headers)
+    return response
+
+
+def get_content(html):
+    soup = BeautifulSoup(html, 'html.parser')
+    items = soup.find_all('div', class_='item product_listbox oh')
+    phones = []
+    for i in items:
+        phones.append({
+            'title': i.find('strong').get_text(),
+            'image': LINK + i.find('img').get('src'),
+            'description': i.find('div', class_='product_text pull-left').get_text().replace('\n\n', ''),
+            'price': i.find('div', class_='listbox_price text-center').get_text().replace('\n', '')
+        })
+    print(phones)
+
+
+def get_parse_result():
+    html = get_html(url=URL, headers=HEADERS)
+    # print(html.text)
+    get_content(html.text)
+
+
+get_parse_result()
